d104ee3ae4a78dfd04dce1a124d098ac6464cd53
danielSilva21 <88036255+danielSilva21@users.noreply.github.com>
2021-11-30 08:59:41 +0100

    User passing

--- website/views.py
+++ website/views.py
@@ -586,7 +586,7 @@ def modifica(rutaPagina,userId):
     print("La ruta de la Pagina es: "+rutaPagina)
     print("El nombre de usuario es: "+userId)
     
-    session['username'] = current_user.email
+    session['username'] = userId
 
     headersUserAgent={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
