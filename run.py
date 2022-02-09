f95c37e1256a99ec4d8dddabfa74028de8e7eb29
danielSilva21 <88036255+danielSilva21@users.noreply.github.com>
2022-02-09 15:52:02 +0100

    Creating Images Containers

--- run.py
+++ run.py
@@ -258,7 +258,7 @@ def main(argv):
 
 
 
-    host = os.environ.get('HOST', '127.0.0.1')
+    host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
     app.run(host=host, port=port,debug=True)
 
